3654dea54a679c7fa290dcf64dde9b2315bdbba3
Akki Reddy <qeakkireddy@gmail.com>
2026-06-27 17:04:16 +0530

    assignment1

---
+++ typecsript-training/assignments/assignment1.ts
@@ -0,0 +1,89 @@
+// 1. Temperature of city in degree celsius : 25.5
+
+        let temp : number = 25.5;
+        console.log (`temperature of the city in deg cel:${temp}`)
+
+// 2 Whether a customer has placed an order: true or false
+
+        let orderplaced : boolean = false;
+        
+        console.log(`customer hase placed the order ${orderplaced}`)
+
+//3. Person's phone number: "123-456-7890"
+
+     let phonenumber : string  = "123-456-7890";
+
+        console.log(`persons phone number is ${phonenumber}`);
+
+// 4. Amount of money in a customer's bank account: 1000.50
+
+         let accountbalance : number = 1000.50;
+         console.log(`Amount of money in customer back account is : ${accountbalance}`);
+
+
+
+// 5. Person's email address: "john.doe@example.com"
+
+          let email : string = "john.doe@example.com";
+          console.log(`Persons email address is : ${email}`);
+
+          
+// 6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+
+       let cordinates : [number,number] = [37.7749, -122.4194]
+
+       console.log(`Latitude is ${cordinates[0]},longitude is ${cordinates[1]}`);
+
+// 7. Person's marital status: true or false
+
+           let maritalstatus : boolean = true
+           console.log(`Person's marital status is :  ${maritalstatus}` )
+
+
+// 8. Person's occupation: "Software Engineer"
+ 
+      let occupation : string = "Software Engineer";
+
+      console.log(`Persons occouupation :${occupation}`)
+
+
+// 9. Person's favourite colour: "Blue"
+
+
+           let colour : string = "Blue"
+
+           console.log(`Persons favorit color :${colour}`);
+
+
+// 10.Current year: 2023
+       
+            let year : number = 2023
+
+            console.log(`Current YEar : ${year}`)
+
+// 11.Number of followers on a social media platform: 1,000,000
+
+               let followers : number = 1000000;
+               console.log(`Number of followers on social media platform are ${followers}`)
+
+
+// 12.Rating of a movie: 7.5
+
+        let rating : number = 7.5
+        console.log(`Rating of a movie is : ${rating}`);
+
+
+
+// 13.Person's blood type: 'A'
+
+
+        let bloodtype : string = 'A'
+        console.log(`Persons blood type is ${bloodtype}`);
+
+        
+// 14.Title of a book: "To Kill a Mockingbird"
+// 15.Number of employees in a company: 500
+// 16.Time of an event: 2:30 PM
+// 17.Name of a country: "United States"
+// 18.Person's eye color: "Brown"
+// 19.Person's birthplace: "New York City");
