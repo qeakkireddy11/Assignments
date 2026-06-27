7ad082bd1cd4aa8f135865e2f7732a3d2cebcdff
Akki Reddy <qeakkireddy@gmail.com>
2026-06-27 10:51:53 +0530

    assignment1

---
+++ javascript-training/debugging.js
@@ -0,0 +1,9 @@
+console.log("Execute line1")
+console.log("Execute line2")
+console.log("Execute line3")
+console.log("Execute line4")
+sumoofnumbers(5,6);
+console.log("Execute line5")
+console.log("Execute line6")
+console.log("Execute line7")
+console.log("Execute line8")
---
+++ javascript-training/hello-world.js
@@ -0,0 +1,5 @@
+console.log("first program")
+
+
+Declaration variable = Data;
+
